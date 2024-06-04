4842b89ea3db23f3076d8a17452c77c46e6c2584
Harald Waxenegger <harald.waxenegger@gmail.com>
2024-06-04 12:54:26 +1000

    address ms compiler warnings

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -89,8 +89,8 @@ std::unique_ptr<ColorMeshGeometry> Geometry::createSphereColorMeshGeometry(const
         if (isBottom) {
             j = lonOffset;
             mesh.indices.emplace_back(j);
-            mesh.indices.emplace_back(mesh.vertices.size()-2);
-            mesh.indices.emplace_back(mesh.vertices.size()-1);
+            mesh.indices.emplace_back(static_cast<uint32_t>(mesh.vertices.size())-2);
+            mesh.indices.emplace_back(static_cast<uint32_t>(mesh.vertices.size())-1);
             while (j<lonOffset+lonIntervals) {
                 mesh.indices.emplace_back(j);
                 mesh.indices.emplace_back(mesh.vertices.size()-1);
